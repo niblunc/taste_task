c95756a68f574db1c1f40fce2778a123fefff039
grace-shearrer <grace.shearrer@gmail.com>
2016-08-31 13:18:26 -0400

    didn't find pump, changed the pump address

--- liquid2.py
+++ liquid2.py
@@ -98,7 +98,8 @@ logfile=logging.LogFile(dataFileName,level=logging.DATA)
 
 try:
     print 'initializing serial device:'
-    dev=syringe_pump.SyringePump('/dev/tty.usbserial')
+    #dev=syringe_pump.SyringePump('/dev/tty.usbserial')
+    dev=syringe_pump.SyringePump('/dev/tty.USA19H142P1.1')
     print dev
     print 'using serial device: ', dev
     if not dev.isOpen():
